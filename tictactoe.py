853d8761335b35bbdf34dfbae7734ec241071ab4
davidlenner <david910128@gmail.com>
2018-02-19 11:18:24 +0100

    table print and shoot

---
+++ tictactoe.py
@@ -0,0 +1,29 @@
+table = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+
+
+def board():
+    for i in range(len(table)):
+        print("|", table[i], "|", end="")
+        if i % 3 == 2:
+            print()
+
+
+def p1shot:
+    k = int(input("number:"))
+    num = table.index(k)
+    if k in range(len(table)):
+        table.insert(num, "X")
+        table.remove(k)
+    board()
+
+
+def p2shot:
+    m = int(input("number:"))
+    num1 = table.index(m)
+    if m in range(len(table)):
+        table.insert(num1, "O")
+        table.remove(m)
+    board()
+
+
+
