5ea82428000badc95816990263f8e8dbe9feafe4
davidlenner <david910128@gmail.com>
2018-02-21 14:23:01 +0100

    we try to print

--- valami.py
+++ valami.py
@@ -1,23 +1,28 @@
 tablesize = int(input("gfdg:"))
-while tablesize >10:
-    tablesize = int(input("gfdg:"))
+
 
 my_list = []
 g = []
 
 
-z = 0
-u = tablesize
+def boardsize():
+    z = 0
+    u = tablesize
+    for x in range(1, (tablesize*tablesize)+1):
+        g.append(x)
+    for i in range(tablesize):    
+        my_list.append(g[z:u])
+        z += tablesize
+        u += tablesize
 
-for x in range(1, (tablesize*tablesize)+1):
-    g.append(x)
-for i in range(tablesize):    
-    my_list.append(g[z:u])
-    z += tablesize
-    u += tablesize
-print("Táblázat:")
-for y in range(0, len(my_list)):
+
+def board():
+    for y in range(0, len(my_list)):
         sor_string = " "
         for x in range(0, len(my_list[y])):
             sor_string += str(my_list[y][x]) + " "
         print(sor_string)
+ 
+
+boardsize()
+board()
