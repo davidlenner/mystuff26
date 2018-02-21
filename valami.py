71e3270eca11c1260ea25403f1da55b1969e9732
davidlenner <david910128@gmail.com>
2018-02-21 13:27:44 +0100

    finally

---
+++ valami.py
@@ -0,0 +1,15 @@
+tablesize = int(input("gfdg:"))
+my_list = []
+g = []
+
+
+z = 0
+u = tablesize
+
+for x in range(1, (tablesize*tablesize)+1):
+    g.append(x)
+for i in range(tablesize):    
+    my_list.append(g[z:u])
+    z += tablesize
+    u += tablesize
+print(my_list)
