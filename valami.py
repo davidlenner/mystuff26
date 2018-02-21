dbd460afbccc85f45508b1a8a9c823ab3cc3b85c
davidlenner <david910128@gmail.com>
2018-02-21 13:32:41 +0100

    printing done

--- valami.py
+++ valami.py
@@ -15,4 +15,9 @@ for i in range(tablesize):
     my_list.append(g[z:u])
     z += tablesize
     u += tablesize
-print(my_list)
+print("Táblázat:")
+for y in range(0, len(my_list)):
+        sor_string = " "
+        for x in range(0, len(my_list[y])):
+            sor_string += str(my_list[y][x]) + " "
+        print(sor_string)
