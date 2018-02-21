fa56f2b5ed6f52e22017d3cd9ce5318c4c03ee99
davidlenner <david910128@gmail.com>
2018-02-21 12:39:55 +0100

    working version

--- tictactoe.py
+++ tictactoe.py
@@ -11,7 +11,7 @@ def board():
                 print()
     if tablesize == 5:
         for i in range(len(table)):
-            print("|","%2s" % table[i], "|", end="")
+            print("|", "%2s" % table[i], "|", end="")
             if (i+1) % 5 == 0:
                 print()
 
@@ -23,21 +23,42 @@ def p1shot():
         table.insert(num, "x")
         table.remove(k)
     board()
-    for i in range(0, 8, 3):
+    for i in range(0, 8):
         if table[i] == "x" and table[i+1] == "x" and table[i+2] == "x":
             print("win")
             exit()
-
+        elif table[i] == "x" and table[i+3] == "x" and table[i+6] == "x":
+            print("win")
+            exit()
+        elif table[i] == "x" and table[i+4] == "x" and table[i+8] == "x":
+            print("win")
+            exit()
+        elif table[i+2] == "x" and table[i+4] == "x" and table[i+6] == "x":
+            print("win")
+            exit()
 
 def p2shot():
     m = int(input("number:"))
     num1 = table.index(m)
     if num1 in range(len(table)):
-        table.insert(num1, "O")
+        table.insert(num1, "0")
         table.remove(m)
     board()
+    for i in range(0, 8):
+        if table[i] == "0" and table[i+1] == "0" and table[i+2] == "0":
+            print("win")
+            exit()
+        elif table[i] == "0" and table[i+3] == "0" and table[i+6] == "0":
+            print("win")
+            exit()
+        elif table[i] == "0" and table[i+4] == "0" and table[i+8] == "0":
+            print("win")
+            exit()
+        elif table[i+2] == "0" and table[i+4] == "0" and table[i+6] == "0":
+            print("win")
+            exit()
 
-
+        
 def boardsize():
     if tablesize == 3:
         while len(table) != tablesize*tablesize:
@@ -47,7 +68,7 @@ def boardsize():
         while len(table) != tablesize*tablesize:
             for i in range(21-(len(table)), 26-(len(table))):
                 table.append(i)
-        
+
 
 table = []
 boardsize()
@@ -61,3 +82,4 @@ while True:
         p2shot()
     except(IndexError, ValueError):
         pass
+
